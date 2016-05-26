91366e0e5730e6b442f62e9eeeede1280d0ee1d0
igonzalezcastro <igonzalezcastro@atanasoff07.danielcastelao.org>
2016-05-26 12:26:22 +0200

    Base de Datos

---
+++ src/base/de/datos/conexion.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package base.de.datos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author igonzalezcastro
+ */
+public class conexion {
+
+    Connection conectar = null;
+
+    public Connection conexion() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conectar = DriverManager.getConnection("jdbc:mysql://10.0.0.254/igonzalezcastro", "igonzalezcastro", "igonzalezcastro");
+            JOptionPane.showMessageDialog(null, "Conectado");
+        } catch (Exception e) {
+            System.out.println("No se ha podido conectar"+e.getMessage());
+        }
+        return conectar;
+    }
+
+}
